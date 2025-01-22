6beffe1090a748b6ea036a0f4ca69408d19ab454
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-01-22 23:36:09 +0900

    v0.3 롤백3

--- backend/src/main/java/com/DokHub/backend/entity/ChannelEntity.java
+++ backend/src/main/java/com/DokHub/backend/entity/ChannelEntity.java
@@ -1,26 +1,26 @@
-package com.DokHub.backend.entity;
-
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@Table(name = "channels", schema = "dokhub") // 명시적으로 테이블 이름 지정
-public class ChannelEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; // PK (Auto-Increment)
-
-    private String category;         // clip, song, main
-    private String channelName;      // 채널 이름
-    private String channelLink;      // 채널 링크
-    private String videoPreviewUrl;  // 썸네일 URL
-
-    // 필요 시 생성자, toString 등 추가
-}
-
+//package com.DokHub.backend.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//@Entity
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@Table(name = "channels", schema = "dokhub") // 명시적으로 테이블 이름 지정
+//public class ChannelEntity {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id; // PK (Auto-Increment)
+//
+//    private String category;         // clip, song, main
+//    private String channelName;      // 채널 이름
+//    private String channelLink;      // 채널 링크
+//    private String videoPreviewUrl;  // 썸네일 URL
+//
+//    // 필요 시 생성자, toString 등 추가
+//}
+//
