c1f18fe65ebf9dddb71435b24cbf6f9819eac37c
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-01-24 21:05:12 +0900

    v0.4 귀찮아 dddsss

--- backend/src/main/java/com/DokHub/backend/service/YouTubeService.java
+++ backend/src/main/java/com/DokHub/backend/service/YouTubeService.java
@@ -16,12 +16,12 @@ import java.util.stream.Collectors;
 
 @Service
 public class YouTubeService {
-    @Value("${youtube.api.key}")
     private String apiKey;
     private final RestTemplate restTemplate;
 
-    public YouTubeService(RestTemplate restTemplate) {
+    public YouTubeService(RestTemplate restTemplate, @Value("${youtube.api.key:}") String apiKey) {
         this.restTemplate = restTemplate;
+        this.apiKey = apiKey;
 
         // 개발환경시 사용
 //        // .env 파일 경로 명시적 지정
@@ -34,7 +34,7 @@ public class YouTubeService {
 //        this.apiKey = dotenv.get("YOUTUBE_API_KEY");
 //        // System.out.println("Loaded API Key: " + this.apiKey); // 로깅 추가
 
-        // API 키가 설정되지 않은 경우 예외 처리
+        // API 키가 없을 경우 예외 발생
         if (this.apiKey == null || this.apiKey.isEmpty()) {
             throw new IllegalStateException("YOUTUBE_API_KEY가 설정되지 않았습니다.");
         }
--- backend/src/test/java/com/DokHub/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/DokHub/backend/BackendApplicationTests.java
@@ -1,13 +1,26 @@
 package com.DokHub.backend;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.web.client.RestTemplate;
+
+import static org.mockito.Mockito.mock;
 
 @SpringBootTest
+@TestPropertySource(properties = {"youtube.api.key=test_api_key"})
 class BackendApplicationTests {
+
+	private RestTemplate restTemplate;
+
+	@BeforeEach
+	void setUp() {
+		restTemplate = mock(RestTemplate.class); // RestTemplate을 Mockito로 모킹
+	}
 
 	@Test
 	void contextLoads() {
+		// 테스트 로직
 	}
-
 }
