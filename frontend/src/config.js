edcf87e705e6fb94c7e7ed1c9f583c1272693814
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-01-26 20:05:58 +0900

    v0.5 eslint 에러 수정

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 // src/config.js
 // export const API_BASE_URL = "http://localhost:8080/api";
-export const API_BASE_URL = "http://52.79.242.58:8080/api";
+// export const API_BASE_URL = "http://52.79.242.58:8080/api";
 // 
