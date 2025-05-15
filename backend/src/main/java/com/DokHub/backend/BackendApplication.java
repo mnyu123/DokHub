593bee7d0b877e9f15d9dc0be96eb18bec3730e1
mnyu123 <whdudwo5582@gmail.com>
2025-05-15 20:06:27 +0900

    v0.9 환경변수 마지막 수정

--- backend/src/main/java/com/DokHub/backend/BackendApplication.java
+++ backend/src/main/java/com/DokHub/backend/BackendApplication.java
@@ -1,7 +1,5 @@
 package com.DokHub.backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -11,16 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching // 캐싱 활성화
 @EnableScheduling // 스케줄링 활성화(2025.02.16)
 public class BackendApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
-
-	@PostConstruct
-	public void testEnvVariables() {
-		// Dotenv 라이브러리를 통해 환경 변수 확인
-		Dotenv dotenv = Dotenv.configure()
-				.directory("src/main/resources")
-				.load();
-		System.out.println("MYSQL_ROOT (Dotenv): " + dotenv.get("MYSQL_ROOT"));
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
