fc3101f4ab08730d9d21d6cc841c32dc65e002c2
mnyu123 <whdudwo5582@gmail.com>
2025-07-20 13:49:42 +0900

    v1.0 아이콘 레이아웃 수정

--- frontend/src/components/CustomLinksSlider.vue
+++ frontend/src/components/CustomLinksSlider.vue
@@ -2,26 +2,35 @@
   <Swiper
     :modules="[Pagination]"
     slides-per-view="5"
-    space-between="48"
+    space-between="32"
     :pagination="{ clickable: true }"
-    class="h-48 py-8 mb-8"
+    initialSlide="2"
+    :allow-touch-move="false"
+    grabCursor
+    centeredSlides
+    class="py-4 pb-16 mb-8 overflow-visible"
   >
     <SwiperSlide
       v-for="(link, idx) in fixedLinks"
       :key="idx"
-      class="rounded-lg overflow-visible shadow tooltip tooltip-bottom"
-      :data-tip="link.alt"
+      class="flex justify-center pb-8 overflow-visible"
     >
-      <a :href="link.href" target="_blank" class="block w-full h-40">
+      <a
+        :href="link.href"
+        target="_blank"
+        class="tooltip tooltip-bottom"
+        :data-tip="link.alt"
+      >
         <img
           :src="link.imgSrc"
-          class="w-full h-full object-cover"
           :alt="link.alt"
+          class="w-16 h-16 object-contain"
         />
       </a>
     </SwiperSlide>
   </Swiper>
 </template>
+
 
 <script setup>
 import { Swiper, SwiperSlide } from 'swiper/vue'
@@ -32,28 +41,28 @@ import 'swiper/css/pagination'
 const fixedLinks = [
   {
     href: 'https://chzzk.naver.com/b68af124ae2f1743a1dcbf5e2ab41e0b',
-    imgSrc: require('@/assets/cafe4.jpg'),
+    imgSrc: require('@/assets/chzzk2.png'),
     alt:    '독케익 치지직 방송'
   },
   {
     href: 'https://cafe.naver.com/poisoncake',
     imgSrc: require('@/assets/cafe5.jpg'),
-    alt:    '개떡이 수용소'
+    alt:    '개떡이 수용소(팬 카페)'
   },
   {
     href: 'https://sharp-scarer-41d.notion.site/1b8d01855ab181989094c50446305706?v=1b8d01855ab180eeb7e5000cac6e922d',
-    imgSrc: require('@/assets/dokart.png'),
+    imgSrc: require('@/assets/doksd.png'),
     alt:    '독케익 예술의 전당'
   },
   {
     href: 'https://thunder-plant-4a0.notion.site/d7cdf50c1aeb4385a4945057caddaf8b',
-    imgSrc: require('@/assets/pngwing.com.png'),
+    imgSrc: require('@/assets/doksd2.png'),
     alt:    '독케익 종합게임 라이브러리'
   },
   {
-    href: 'https://www.notion.so/1e9111b4b3f780e18878e3b0750b6c6f?v=1e9111b4b3f7803abc5e000cf6fb18ca',
-    imgSrc: require('@/assets/pngwing.com.png'),
-    alt:    '독케익 방송일정'
+    href: 'https://www.youtube.com/@%EB%8F%85%EC%BC%80%EC%9D%B5%EC%9C%A0%ED%8A%9C%EB%B8%8C',
+    imgSrc: require('@/assets/youtube_logo.png'),
+    alt:    '독케익 유튜브'
   }
 ]
 </script>
