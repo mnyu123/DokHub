6acaa0e1afea0825901465d86e07f9256c93f9f6
mnyu123 <whdudwo5582@gmail.com>
2025-07-26 16:34:14 +0900

    v1.0 아이콘 업데이트(AI 이미지 사용) , 레이아웃 수정 , 업데이트 박스 내용 변경

--- frontend/src/components/CustomLinksSlider.vue
+++ frontend/src/components/CustomLinksSlider.vue
@@ -7,13 +7,14 @@
     initialSlide="2"
     :allow-touch-move="false"
     grabCursor
-    centeredSlides
     class="py-4 pb-16 mb-8 overflow-visible"
+    style="overflow: visible;"
   >
     <SwiperSlide
       v-for="(link, idx) in fixedLinks"
       :key="idx"
       class="flex justify-center pb-8 overflow-visible"
+      style="overflow: visible;"
     >
       <a
         :href="link.href"
@@ -24,19 +25,12 @@
         <img
           :src="link.imgSrc"
           :alt="link.alt"
-          :class="[
-            'object-contain transition-transform',
-            // 기본 사이즈
-            'w-16 h-32',
-            // 개떡이 수용소만 살짝 크게
-            link.alt === '개떡이 수용소(팬 카페)' ? 'w-48 h-32' : ''
-          ]"
+          class="object-contain transition-transform w-32 h-32"
         />
       </a>
     </SwiperSlide>
   </Swiper>
 </template>
-
 
 <script setup>
 import { Swiper, SwiperSlide } from 'swiper/vue'
@@ -47,27 +41,27 @@ import 'swiper/css/pagination'
 const fixedLinks = [
   {
     href: 'https://chzzk.naver.com/b68af124ae2f1743a1dcbf5e2ab41e0b',
-    imgSrc: require('@/assets/chzzk2.png'),
+    imgSrc: require('@/assets/chzzk_ico.png'),
     alt:    '독케익 치지직 방송'
   },
   {
     href: 'https://cafe.naver.com/poisoncake',
-    imgSrc: require('@/assets/cafe5.jpg'),
+    imgSrc: require('@/assets/new_cafe_ico.jpg'),
     alt:    '개떡이 수용소(팬 카페)'
   },
   {
     href: 'https://sharp-scarer-41d.notion.site/1b8d01855ab181989094c50446305706?v=1b8d01855ab180eeb7e5000cac6e922d',
-    imgSrc: require('@/assets/doksd.png'),
+    imgSrc: require('@/assets/dok_fanArt_ico.png'),
     alt:    '독케익 예술의 전당'
   },
   {
     href: 'https://thunder-plant-4a0.notion.site/d7cdf50c1aeb4385a4945057caddaf8b',
-    imgSrc: require('@/assets/doksd2.png'),
+    imgSrc: require('@/assets/dok_game_lib_ico.png'),
     alt:    '독케익 종합게임 라이브러리'
   },
   {
     href: 'https://www.youtube.com/@%EB%8F%85%EC%BC%80%EC%9D%B5%EC%9C%A0%ED%8A%9C%EB%B8%8C',
-    imgSrc: require('@/assets/youtube_logo.png'),
+    imgSrc: require('@/assets/youtube_ico.png'),
     alt:    '독케익 유튜브'
   }
 ]
--- frontend/src/components/UpdateBox.vue
+++ frontend/src/components/UpdateBox.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card bg-base-100 shadow-md">
     <div class="card-body p-4">
-      <h2 class="card-title text-lg">v 0.9 → 1.0  업데이트</h2>
+      <h2 class="card-title text-lg">1.0 업데이트</h2>
       <ul class="list-disc list-inside text-sm leading-6">
         <li>독채팅(Beta) 추가</li>
         <li>업로드 중단 채널 탭 분리</li>
