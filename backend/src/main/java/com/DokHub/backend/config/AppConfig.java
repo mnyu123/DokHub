e666678558236246913b7c2aec16fcaf7caac1f1
mnyu123 <whdudwo5582@gmail.com>
2026-05-04 19:51:37 +0900

    v1.3 키 존재 체크용 내용 추가

--- backend/src/main/java/com/DokHub/backend/config/AppConfig.java
+++ backend/src/main/java/com/DokHub/backend/config/AppConfig.java
@@ -14,7 +14,7 @@ public class AppConfig {
 
     @PostConstruct
     public void testEnvVariables() {
-        System.out.println("[유튜브 키 환경변수 안씀]: " + System.getenv("YOUTUBE_API_KEY"));
-        System.out.println("[DB비번 환경변수 안씀]: " + System.getenv("MYSQL_ROOT"));
+        System.out.println("[YOUTUBE_API_KEY 존재여부]: " + (System.getenv("YOUTUBE_API_KEY") != null));
+        System.out.println("[DB_PASSWORD 존재여부]: " + (System.getenv("DB_PASSWORD") != null));
     }
 }
